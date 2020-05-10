d0480deee7585eeb2af4f7a0c528ae27c682edb5
root <root@localhost.localdomain>
2020-05-10 11:31:04 +0000

    initial

---
+++ recipes/setup.rb
@@ -0,0 +1,14 @@
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Jonas'
+  owner 'root'
+  group 'root'
+end
+
